2a1ad94894e7a88feb2059bcfdf57df623cfa58c
Leonel Herrera Alsina <leonelhalsina@gmail.com>
2019-06-25 10:30:03 +0000

    version 2.0.0

--- R/cla_secsse_ml_func_def_pars.R
+++ R/cla_secsse_ml_func_def_pars.R
@@ -1,6 +1,5 @@
-
-#' Maximum likehood estimation under Several examined and concealed States-dependent Speciation and Extinction (SecSSE) where some paramaters are functions of other parameters and/or factors.
-#' @title Maximum likehood estimation for (SecSSE) with parameter as complex functions.
+#' Maximum likehood estimation under cla Several examined and concealed States-dependent Speciation and Extinction (SecSSE) where some paramaters are functions of other parameters and/or factors. Offers the option of cladogenesis
+#' @title Maximum likehood estimation for (SecSSE) with parameter as complex functions. Cladogenetic version
 #' @param phy phylogenetic tree of class phylo, ultrametric, rooted and with branch lengths.
 #' @param traits a vector with trait states for each tip in the phylogeny.
 #' @param num_concealed_states number of concealed states, generally equivalent to the number of examined states in the dataset.
@@ -9,7 +8,7 @@
 #' @param initparsopt initial guess of the parameters to be estimated.
 #' @param idfactorsopt id of the factors that will be optimized. There are not fixed factors, so use a constant within 'functions_defining_params'.
 #' @param initfactors the initial guess for a factor (it should be set to NULL when no factors).
-#' @param idparsfix id of the fixed parameters (it should be set to NULL when there are no factors).
+#' @param idparsfix id of the fixed parameters (it should be set to NULL when no factors).
 #' @param parsfix value of the fixed parameters.
 #' @param idparsfuncdefpar id of the parameters which will be a function of optimized and/or fixed parameters. The order of id should match functions_defining_params
 #' @param functions_defining_params a list of functions. Each element will be a function which defines a parameter e.g. id_3 <- (id_1+id_2)/2. See example and vigenette
@@ -24,7 +23,7 @@
 #' @param num_cycles number of cycles of the optimization (default is 1).
 #' @param run_parallel should the routine to run in parallel be called? Read note below
 #' @param loglik_penalty the size of the penalty for all parameters; default is 0 (no penalty)
-#' @note To run in parallel it is needed to load the following libraries when windows: apTreeshape, doparallel and foreach. When unix, it requires: apTreeshape, doparallel, foreach and doMC
+#' @note To run in parallel the following libraries must be loaded under windows: apTreeshape, doparallel and foreach. Under unix/linux, apTreeshape, doparallel, foreach and doMC must be loaded.
 #' @return Parameter estimated and maximum likelihood
 #' @examples
 #'# Example of how to set the arguments for a ML search.
@@ -33,17 +32,15 @@
 #'library(DDD)
 #'set.seed(16)
 #'phylotree <- ape::rbdtree(0.07,0.001,Tmax=50)
-#'startingpoint<-bd_ML(brts = ape::branching.times(phylotree))
+#'startingpoint <- bd_ML(brts = ape::branching.times(phylotree))
 #'intGuessLamba <- startingpoint$lambda0
 #'intGuessMu <- startingpoint$mu0
-#'traits <- sample(c(0,1,2), ape::Ntip(phylotree),replace=TRUE) #get some traits
-#'num_concealed_states<-3
-#'idparslist<-id_paramPos(traits, num_concealed_states)
-#'idparslist[[1]][c(1,4,7)] <- 1
-#'idparslist[[1]][c(2,5,8)] <- 2
-#'idparslist[[1]][c(3,6,9)] <- 3
-#'idparslist[[2]][]<-4
-#'masterBlock <- matrix(c(5,6),ncol = 3,nrow = 3,byrow = TRUE)
+#'traits <-  sample(c(0,1,2), ape::Ntip(phylotree),replace=TRUE) #get some traits
+#'num_concealed_states <- 3
+#'idparslist <- cla_id_paramPos(traits,num_concealed_states)
+#'idparslist$lambdas[1,] <- c(1,2,3,1,2,3,1,2,3)
+#'idparslist[[2]][] <- 4
+#'masterBlock <- matrix(c(5,6),ncol=3,nrow=3,byrow=TRUE)
 #'diag(masterBlock) <- NA
 #'diff.conceal <- FALSE
 #'idparslist[[3]] <- q_doubletrans(traits,masterBlock,diff.conceal)
@@ -59,8 +56,8 @@
 #'# function is defined, be sure that all the parameters involved are either estimated, fixed or
 #'# defined by previous functions (i.e, a function that defines parameter in
 #'# 'functions_defining_params'). The user is responsible for this. In this example, par_3
-#'# (i.e., parameter 3) is needed to calculate par_6. This is correct because par_3 is defined in
-#'# the first function of 'functions_defining_params'. Notice that factor_1 indicates a value
+#'# (i.e., parameter 3) is needed to calculate par_6. This is correct because par_3 is defined
+#'# in the first function of 'functions_defining_params'. Notice that factor_1 indicates a value
 #'# that will be estimated to satisfy the equation. The same factor can be shared to define
 #'# several parameters.
 #'functions_defining_params <- list()
@@ -73,7 +70,6 @@
 #'functions_defining_params[[3]] <- function(){
 #'  par_6 <- par_3 * factor_1
 #'}
-
 #'
 #'tol = c(1e-04, 1e-05, 1e-07)
 #'maxiter = 1000 * round((1.25)^length(idparsopt))
@@ -81,19 +77,20 @@
 #'methode = "ode45"
 #'optimmethod = "simplex"
 #'run_parallel = FALSE
-#'cond<-"proper_cond"
+#'cond <- "proper_cond"
 #'root_state_weight <- "proper_weights"
 #'sampling_fraction <- c(1,1,1)
-#'#model <- secsse_ml_func_def_pars(phylotree, traits, num_concealed_states, idparslist, idparsopt,
-#'#                               initparsopt, idfactorsopt, initfactors, idparsfix, parsfix,
-#'#                               idparsfuncdefpar, functions_defining_params, cond,
-#'#                               root_state_weight, sampling_fraction, tol, maxiter, use_fortran,
-#'#                               methode, optimmethod, num_cycles = 1,run_parallel)
+#'#model <- cla_secsse_ml_func_def_pars(phylotree, traits, num_concealed_states, idparslist,
+#'#                                   idparsopt, initparsopt, idfactorsopt, initfactors,
+#'#                                   idparsfix, parsfix, idparsfuncdefpar,
+#'#                                   functions_defining_params, cond, root_state_weight,
+#'#                                   sampling_fraction, tol, maxiter, use_fortran,
+#'#                                   methode, optimmethod, num_cycles = 1,run_parallel)
 #'
 #'# ML -136.5796
 #' @export
 
-secsse_ml_func_def_pars <- function(phy,
+cla_secsse_ml_func_def_pars <- function(phy,
                                     traits,
                                     num_concealed_states,
                                     idparslist,
@@ -111,7 +108,7 @@ secsse_ml_func_def_pars <- function(phy,
                                     tol = c(1E-4, 1E-5, 1E-7),
                                     maxiter = 1000 * round((1.25) ^ length(idparsopt)),
                                     use_fortran = TRUE,
-                                    methode = "ode45",
+                                    methode = 'ode45',
                                     optimmethod = 'simplex',
                                     num_cycles = 1,
                                     run_parallel = FALSE,
@@ -120,33 +117,29 @@ secsse_ml_func_def_pars <- function(phy,
   structure_func <- list()
   structure_func[[1]] <- idparsfuncdefpar
   structure_func[[2]] <- functions_defining_params
-  if(is.null(idfactorsopt)){
-    structure_func[[3]] <- "noFactor"
-  } else {
-    structure_func[[3]] <- idfactorsopt
-  }
+  structure_func[[3]] <- idfactorsopt
   
   see_ancestral_states <- FALSE
   if (is.null(idfactorsopt) == FALSE) {
     if (length(initfactors) != length(idfactorsopt)) {
-      stop("idfactorsopt should have the same length as initfactors.")
+      stop("idfactorsopt should have the same length than initfactors.")
     }
   }
   
   if (is.list(functions_defining_params) == FALSE) {
     stop(
-      "The argument functions_defining_params should be a list of functions. See example and vignette"
+      "the argument functions_defining_params should be a list of functions. See example and vignette"
     )
   }
   
   if (length(functions_defining_params) != length(idparsfuncdefpar)) {
     stop(
-      "The argument functions_defining_params should have the same length than idparsfuncdefpar"
+      "the argument functions_defining_params should have the same length as idparsfuncdefpar"
     )
   }
   
   if (is.matrix(traits)) {
-    cat("You are setting a model where some species had more than one trait state \n")
+    cat("you are setting a model where some species had more than one trait state \n")
   }
   
   if (length(initparsopt) != length(idparsopt)) {
@@ -162,7 +155,7 @@ secsse_ml_func_def_pars <- function(phy,
   }
   
   if (anyDuplicated(c(idparsopt, idparsfix, idparsfuncdefpar)) != 0) {
-    stop("At least one element was asked to be fixed, estimated or a function at the same time")
+    stop("at least one element was asked to be fixed, estimated or a function at the same time")
   }
   
   if (identical(as.numeric(sort(
@@ -181,6 +174,8 @@ secsse_ml_func_def_pars <- function(phy,
     cat("You set some transitions as impossible to happen", "\n")
   }
   
+  idparslist[[1]] <- prepare_full_lambdas(traits,num_concealed_states,idparslist[[1]])
+  see_ancestral_states <- FALSE 
   
   #options(warn = -1)
   cat("Calculating the likelihood for the initial parameters.", "\n")
@@ -193,15 +188,12 @@ secsse_ml_func_def_pars <- function(phy,
   trparsfix <- parsfix / (1 + parsfix)
   trparsfix[which(parsfix == Inf)] <- 1
   
-  
   optimpars <- c(tol, maxiter)
-  
-  
-  
+
   if (.Platform$OS.type == "windows" && run_parallel == TRUE) {
     cl <- parallel::makeCluster(2)
     doParallel::registerDoParallel(cl)
-    setting_calculation <-
+    setting_calculation <- 
       build_initStates_time_bigtree(phy, traits, num_concealed_states, sampling_fraction)
     setting_parallel <- 1
     on.exit(parallel::stopCluster(cl))
@@ -209,19 +201,18 @@ secsse_ml_func_def_pars <- function(phy,
   
   if (.Platform$OS.type == "unix" && run_parallel == TRUE) {
     doMC::registerDoMC(2)
-    setting_calculation <-
+    setting_calculation <- 
       build_initStates_time_bigtree(phy, traits, num_concealed_states, sampling_fraction)
     setting_parallel <- 1
   }
   
   if (run_parallel == FALSE) {
-    setting_calculation <-
+    setting_calculation <- 
       build_initStates_time(phy, traits, num_concealed_states, sampling_fraction)
     setting_parallel <- NULL
   }
   
-  
-  initloglik <-
+  initloglik <- 
     secsse_loglik_choosepar(
       trparsopt = trparsopt,
       trparsfix = trparsfix,
@@ -231,8 +222,7 @@ secsse_ml_func_def_pars <- function(phy,
       structure_func = structure_func,
       phy = phy,
       traits = traits,
-      num_concealed_states =
-        num_concealed_states,
+      num_concealed_states = num_concealed_states,
       use_fortran = use_fortran,
       methode = methode,
       cond = cond,
@@ -257,7 +247,7 @@ secsse_ml_func_def_pars <- function(phy,
     cat("Optimizing the likelihood - this may take a while.", "\n")
     utils::flush.console()
     cat(setting_parallel, "\n")
-    out <-
+    out <- 
       DDD::optimizer(
         optimmethod = optimmethod,
         optimpars = optimpars,
@@ -287,7 +277,7 @@ secsse_ml_func_def_pars <- function(phy,
     {
       stop("Optimization has not converged. Try again with different initial values.\n")
     } else {
-      MLpars1 <-
+      MLpars1 <- 
         secsse_transform_parameters(
           as.numeric(unlist(out$par)),
           trparsfix,
@@ -296,7 +286,7 @@ secsse_ml_func_def_pars <- function(phy,
           idparslist,
           structure_func
         )
-      out2 <-
+      out2 <- 
         list(MLpars = MLpars1, ML = as.numeric(unlist(out$fvalues)),conv = out$conv)
     }
   }
